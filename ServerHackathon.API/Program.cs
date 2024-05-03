7bbb1cecf3ba92fe59505c5c44bd404dbe045f52
Efremov Alexandr <sasha_efremov123@mail.ru>
2024-05-03 20:27:37 +0300

    Run Migrate

--- ServerHackathon.API/Program.cs
+++ ServerHackathon.API/Program.cs
@@ -53,6 +53,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var dataContext = scope.ServiceProvider.GetRequiredService<DataContext>();
+    dataContext.Database.Migrate();
 }
 
 // Configure the HTTP request pipeline.
