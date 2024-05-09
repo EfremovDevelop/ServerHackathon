f4fcd1587c2a4a418b5e3546894dbd505f7517c1
XvisionPro <dubov.study37@outlook.com>
2024-05-09 22:14:45 +0300

    Add image

--- ServerHackathon.API/Controllers/UsersController.cs
+++ ServerHackathon.API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ServerHackathon.API.Contracts.Users;
 using ServerHackathon.Application.Services;
@@ -11,15 +12,43 @@ namespace ServerHackathon.API.Controllers
     public class UsersController : ControllerBase
     {
         private readonly UsersService _usersService;
+        public static IWebHostEnvironment  _webHostEnvironment;
 
-        public UsersController(UsersService usersService)
+        public UsersController(UsersService usersService, IWebHostEnvironment webHostEnvironment)
         {
             _usersService = usersService;
+            _webHostEnvironment = webHostEnvironment;
+            if (string.IsNullOrWhiteSpace(_webHostEnvironment.WebRootPath))
+            {
+                Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "wwwroot");
+            }
         }
 
         [HttpPost("register")]
-        public async Task<IResult> Register([FromBody] UsersRequest usersRequest)
+        public async Task<IResult> Register([FromForm] UsersRequest usersRequest)
         {
+            //Avatar Validation
+            string profileImageUrl = "";
+            if (usersRequest.ProfileImageUrl.Length>0)
+            {
+                string path = _webHostEnvironment.WebRootPath + "\\uploads\\";
+                if(!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+
+                }
+                using (FileStream fileStream = System.IO.File.Create(path + usersRequest.ProfileImageUrl.FileName))
+                {
+                    usersRequest.ProfileImageUrl.CopyTo(fileStream);
+                    fileStream.Flush();
+                    profileImageUrl = "\\static\\uploads\\" + usersRequest.ProfileImageUrl.FileName;
+                }
+            }
+            else
+            {
+                return Results.BadRequest("Not Loaded Image");
+            }
+
             var userDto = new UserDto();
             userDto.Name = usersRequest.Name;
             userDto.Surname = usersRequest.Surname;
@@ -28,6 +57,8 @@ namespace ServerHackathon.API.Controllers
             userDto.Password = usersRequest.Password;
             userDto.GenderId = usersRequest.GenderId;
             userDto.Phone = usersRequest.Phone;
+            userDto.UniversityId = usersRequest.UniversityId;
+            userDto.ProfileImageUrl = profileImageUrl;
 
             var id = await _usersService.Register(userDto);
             if (id == null)
@@ -51,9 +82,11 @@ namespace ServerHackathon.API.Controllers
             {
                 return NotFound("User not found.");
             }
+            var baseUri = $"{Request.Scheme}://{Request.Host}";
+            var path = baseUri+user.ProfileImageUrl.Replace("\\", "/");
 
             var response = new UsersResponse(user.Id, user.Name, user.Surname, user.Login,
-                user.GenderId, user.Phone, user.Email);
+                user.GenderId, user.Phone, user.Email, path);
 
             return Ok(response);
         }
@@ -76,6 +109,24 @@ namespace ServerHackathon.API.Controllers
         public ActionResult<string> GetSecureResource()
         {
             return Ok();
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("static")]
+        public IActionResult Get(string id)
+        {
+            
+            var path = Path.Combine(_webHostEnvironment.WebRootPath, "uploads", id);
+            try
+            {
+                var imageFileStream = System.IO.File.OpenRead(path);
+                return File(imageFileStream, "image/jpeg");
+                
+            }
+            catch (System.Exception)
+            {
+                return NotFound();
+            }
         }
     }
 }
